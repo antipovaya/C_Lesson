cbfd5a2cfc2decd048ae005a22b78fd74b9a5243
Yana Antipova <antipovayana01@gmail.com>
2023-06-09 14:42:32 +0300

    Урок

---
+++ Lesson001/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Console.WriteLine("Today is Friday");
+Console.Write("Today is friday");
+
+Console.ReadLine(); // string // Нужно ли переводить строку в число? Да.
+int number = Convert.ToInt32(Console.ReadLine());
+*/
+/*Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(number);
+Console.WriteLine($"your number is {number}");
+// Задача 1.
+//Напишите программу, которая на вход принимает число и выдаёт его квадрат*/
+
+
+/*Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine(result);
+*/
+//Задача 3. 
+//Напишите программу, которая на вход принимает два числа и проверяет, 
+//является ли первое число квадратом второго.
+
+Console.WriteLine("Enter first number: ");
+Console.WriteLine("Enter second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2 * number2);
+{
+    Console.WriteLine("First number is a square of the second one");
+}
+else
+{
+    Console.WriteLine(number1 + "is not a square of the" + number2);
+}
