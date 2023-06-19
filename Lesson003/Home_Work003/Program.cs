f2a2a84eb8fab0cfdf6a093baf0fc81c079a284c
Yana Antipova <antipovayana01@gmail.com>
2023-06-19 15:50:12 +0300

    Задача_21_23

--- Lesson003/Home_Work003/Program.cs
+++ Lesson003/Home_Work003/Program.cs
@@ -4,9 +4,9 @@
 //12821 -> да
 //23432 -> да
 
-Console.WriteLine("Введите пятизначное число: ");
-int uzer_number = Convert.ToInt32(Console.ReadLine());
 
+
+/*
 void getNumberFive(int x){
 
 if (x > 99999 || x < 10000){
@@ -32,11 +32,85 @@ else {
     }
 }
 }
+Console.WriteLine("Введите пятизначное число: ");
+int uzer_number = Convert.ToInt32(Console.ReadLine());
 getNumberFive (uzer_number);
 
+*/
 
 //Задача 21
 //Напишите программу, которая принимает на вход координаты 
 //двух точек и находит расстояние между ними в 3D пространстве.
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
+
+/*
+double getLongLineAB (int xA, int xB, int yA, int yB, int zA, int zB){
+
+    double longLine = Math.Sqrt((xB - xA)*(xB - xA) + (yB - yA)*(yB - yA) + (zB - zA)*(zB - zA));
+   
+    return longLine;
+}
+
+Console.WriteLine("Input number X coord of A: ");
+int xCoordA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number Y coord of A: ");
+int yCoordA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number Z coord of A: ");
+int zCoordA = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Input number X coord of B: ");
+int xCoordB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number Y coord of B: ");
+int yCoordB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input number Z coord of B: ");
+int zCoordB = Convert.ToInt32(Console.ReadLine());
+
+
+
+double dist = getLongLineAB (xCoordA, xCoordB, yCoordA, yCoordB, zCoordA, zCoordB);
+Console.WriteLine($"Distance between A and B is {dist:f2}");
+
+*/
+
+//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+void getCube (int numberN){
+
+if (numberN > 0){
+    int current = 1;
+    while (current <= numberN){
+
+     double res = Math.Pow(current, 3);
+      Console.Write($"{res:f0}, ");
+     current++;
+    }
+}
+else {
+
+Console.Write("1, 0, ");
+
+int current = -1;
+    while (current >= numberN){
+
+     double res = Math.Pow(current, 3);
+      Console.Write($"{res:f0}, ");
+     current--;
+    }
+
+}
+
+Console.WriteLine("\b\b.");
+}
+Console.WriteLine("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+getCube (number);
