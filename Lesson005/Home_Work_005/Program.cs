aa98fd8e57899339c1905c5f359c33eaf85c0f49
Yana Antipova <antipovayana01@gmail.com>
2023-06-26 12:33:43 +0300

    Задача домашняя 38

--- Lesson005/Home_Work_005/Program.cs
+++ Lesson005/Home_Work_005/Program.cs
@@ -2,92 +2,94 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-/*
-int [] CreatArray (){
 
-    int size = new Random().Next(10, 41);
-    int [] newArray = new int [size];
+// int [] CreatArray (){
 
-    for (int i = 0; i < size; i++)
-    {
-        newArray [i] = new Random().Next(100, 1000);
-    }
+//     int size = new Random().Next(10, 41);
+//     int [] newArray = new int [size];
 
-    return newArray;
-}
-*/
-/*
-void PrintArray (int [] arrayToShow){
+//     for (int i = 0; i < size; i++)
+//     {
+//         newArray [i] = new Random().Next(100, 1000);
+//     }
 
-     Console.WriteLine ("Ваш массив: ");
+//     return newArray;
+// }
 
-    for (int i = 0; i < arrayToShow.Length; i++)
-    {
-        Console.Write (arrayToShow[i] + " ");
-    }
+
+// void PrintArray (int [] arrayToShow){
+
+//      Console.WriteLine ("Ваш массив: ");
+
+//     for (int i = 0; i < arrayToShow.Length; i++)
+//     {
+//         Console.Write (arrayToShow[i] + " ");
+//     }
    
-}
-*/
-/*
-void EvenNumber (int [] arrayCreat){
+// }
 
-    int count = 0;
-    for (int i = 0; i < arrayCreat.Length; i++){
-        if (arrayCreat[i] % 2 == 0){
-            count++;
-        }
-    }
 
-    Console.Write($"-> {count}");
-}
+// void EvenNumber (int [] arrayCreat){
 
-int [] Array = CreatArray();
-PrintArray(Array);
-EvenNumber(Array);
-*/
+//     int count = 0;
+//     for (int i = 0; i < arrayCreat.Length; i++){
+//         if (arrayCreat[i] % 2 == 0){
+//             count++;
+//         }
+//     }
+
+//     Console.Write($"-> {count}");
+// }
+
+// int [] Array = CreatArray();
+// PrintArray(Array);
+// EvenNumber(Array);
+
 
 // Задача 36: Задайте одномерный массив, заполненный 
 // случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
-int [] CreateArray(){
 
-    int size = new Random().Next(5, 8);
-    int [] CreateNewArray = new int [size];
 
-    for (int i = 0; i < CreateNewArray.Length; i++){
-        CreateNewArray[i] = new Random().Next(-5, 10);
-    }
-    return CreateNewArray;
-}
+// int [] CreateArray(){
 
-void ShowArray (int [] arrayToShow){
+//     int size = new Random().Next(5, 8);
+//     int [] CreateNewArray = new int [size];
 
-     Console.WriteLine ("Ваш массив: ");
+//     for (int i = 0; i < CreateNewArray.Length; i++){
+//         CreateNewArray[i] = new Random().Next(-5, 10);
+//     }
+//     return CreateNewArray;
+// }
 
-    for (int i = 0; i < arrayToShow.Length; i++)
-    {
-        Console.Write (arrayToShow[i] + " ");
-    }
+// void ShowArray (int [] arrayToShow){
+
+//      Console.WriteLine ("Ваш массив: ");
+
+//     for (int i = 0; i < arrayToShow.Length; i++)
+//     {
+//         Console.Write (arrayToShow[i] + " ");
+//     }
    
-}
+// }
 
-void UnevenPosSumm (int [] arrayCreate){
+// void UnevenPosSumm (int [] arrayCreate){
 
-    int summ = 0;
-    for (int i = 0; i < arrayCreate.Length; i++){
+//     int summ = 0;
+//     for (int i = 0; i < arrayCreate.Length; i++){
 
-        if (i % 2 > 0){
-            summ += arrayCreate[i];
-        }
-    }
-    Console.Write($"-> {summ}");
-}
+//         if (i % 2 > 0){
+//             summ += arrayCreate[i];
+//         }
+//     }
+//     Console.Write($"-> {summ}");
+// }
 
-int [] Array = CreateArray();
-ShowArray (Array);
-UnevenPosSumm (Array);
+// int [] Array = CreateArray();
+// ShowArray (Array);
+// UnevenPosSumm (Array);
 
 
 // Задача 38: Задайте массив вещественных чисел. 
@@ -96,3 +98,53 @@ UnevenPosSumm (Array);
 
 
 
+// double [] CreateArray(){
+
+//     int size = new Random().Next(5, 16);
+//     double [] CreateNewArray = new double [size];
+
+//     for (int i = 0; i < CreateNewArray.Length; i++){
+//         CreateNewArray[i] = new Random().Next(0, 101) + new Random().NextDouble();
+
+//     }
+//     return CreateNewArray;
+// }
+
+// void ShowArray (double [] arrayToShow){
+
+//      Console.WriteLine ("Ваш массив: ");
+
+//     for (int i = 0; i < arrayToShow.Length; i++)
+//     {
+//         Console.Write ($"{arrayToShow[i]:f3} ");
+//     }
+   
+// }
+
+
+// void MinMaxPos (double [] ArrayRandom){
+//     double minPos = ArrayRandom[0];
+//     double maxPos = ArrayRandom[0];
+
+//     for (int i = 0; i < ArrayRandom.Length; i++){
+
+//         if (ArrayRandom[i] > maxPos){
+//             maxPos = ArrayRandom[i];
+//         }
+//         else
+//         if(ArrayRandom[i] < minPos){
+//             minPos = ArrayRandom[i];
+//         }
+//     }
+
+//     double diff = maxPos - minPos;
+//     Console.Write($"-> {diff:f3}");
+//     Console.WriteLine();
+//     Console.WriteLine($"В массиве определены минимальное значение: {minPos:f3} и максимальное значение: {maxPos:f3}");
+//     Console.WriteLine($"Разница между ними составила: {diff:f3}");
+// }
+
+
+// double [] Array = CreateArray();
+// ShowArray(Array);
+// MinMaxPos(Array);
