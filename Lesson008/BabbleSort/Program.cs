7662094e5ca81e6133c87a240d6d7f223940f3c9
Yana Antipova <antipovayana01@gmail.com>
2023-07-06 17:15:10 +0300

    Задача домашняя 54

--- Lesson008/BabbleSort/Program.cs
+++ Lesson008/BabbleSort/Program.cs
@@ -1,5 +1,62 @@
 ﻿// Решение задачи на сортировку массива полностью
 
+int [,] CreateArray2D (){
+
+    int m = new Random().Next(3, 5);
+    int n = new Random().Next(3, 5);
+
+    int [,] creatArr = new int [m, n];
+    for (int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++)
+        {
+            creatArr[i,j] = new Random().Next(0, 10);
+        }
+    }
+    return creatArr;
+ }
+
+void Print (int [,] array2D){
+    Console.WriteLine("Ваш массив: ");
+    Console.WriteLine();
+
+    for(int i = 0; i < array2D.GetLength(0); i++){
+        for(int j = 0; j < array2D.GetLength(1); j++){
+            
+            Console.Write(array2D[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+Console.WriteLine();
+}
+
+
+
+// int [,] ArrangeArray (int [,] arrayRandom)
+// {
+//     int n = arrayRandom.GetLength(0); //строки
+//     int m = arrayRandom.GetLength(1); //столбцы
+//     //int [,] arrayCorrect = new int[n,m];
+//     int max = arrayRandom[0,0];
+
+        
+//             for (int i = 0; i < n; i++)
+//                     for(int j = 0; j < m; j++)
+//                         for (int x = 0; x < n ; x++)
+//                             for (int y = 0; y < m ; y++)
+
+//                             if (arrayRandom[x,y] > arrayRandom[i,j])
+//                             {
+//                                 max = arrayRandom[x,y];
+//                                 arrayRandom[x,y] = arrayRandom[i,j];
+//                                 arrayRandom[i,j] = max;
+//                             }          
+// return arrayRandom;
+// }  
+
+// int [,] arr = CreateArray2D();
+// Print(arr);
+// Print(ArrangeArray(arr));
+
 
 int [,] ArrangeArray (int [,] arrayRandom)
 {
@@ -11,18 +68,18 @@ int [,] ArrangeArray (int [,] arrayRandom)
         
             for (int i = 0; i < n; i++)
                     for(int j = 0; j < m; j++)
-                        for (int x = 0; x < n ; x++)
+                        //for (int x = 0; x < n ; x++)
                             for (int y = 0; y < m ; y++)
 
-                            if (arrayRandom[x,y] > arrayRandom[i,j])
+                            if (arrayRandom[i,y] > arrayRandom[i,j])
                             {
-                                max = arrayRandom[x,y];
-                                arrayRandom[x,y] = arrayRandom[i,j];
+                                max = arrayRandom[i,y];
+                                arrayRandom[i,y] = arrayRandom[i,j];
                                 arrayRandom[i,j] = max;
                             }          
 return arrayRandom;
 }  
 
-// int [,] arr = CreateArray2D();
-// Print(arr);
-// Print(ArrangeArray(arr));
+int [,] arr = CreateArray2D();
+Print(arr);
+Print(ArrangeArray(arr));
--- Lesson008/Home_Work008/Program.cs
+++ Lesson008/Home_Work008/Program.cs
@@ -9,8 +9,6 @@
 // 8 4 4 2
 
 
-
-
  int [,] CreateArray2D (){
 
     int m = new Random().Next(3, 5);
@@ -39,24 +37,36 @@ void Print (int [,] array2D){
     }
 Console.WriteLine();
 }
+void Print2 (int [,] array2D){
+    Console.WriteLine("Ваш сортированный построчно массив: ");
+    Console.WriteLine();
+
+    for(int i = 0; i < array2D.GetLength(0); i++){
+        for(int j = 0; j < array2D.GetLength(1); j++){
+            
+            Console.Write(array2D[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+Console.WriteLine();
+}
+
 
 int [,] ArrangeArray (int [,] arrayRandom)
 {
     int n = arrayRandom.GetLength(0); //строки
     int m = arrayRandom.GetLength(1); //столбцы
-    //int [,] arrayCorrect = new int[n,m];
     int max = arrayRandom[0,0];
 
         
             for (int i = 0; i < n; i++)
                     for(int j = 0; j < m; j++)
-                        for (int x = 0; x < n ; x++)
                             for (int y = 0; y < m ; y++)
 
-                            if (arrayRandom[x,y] > arrayRandom[i,j])
+                            if (arrayRandom[i,y] > arrayRandom[i,j])
                             {
-                                max = arrayRandom[x,y];
-                                arrayRandom[x,y] = arrayRandom[i,j];
+                                max = arrayRandom[i,y];
+                                arrayRandom[i,y] = arrayRandom[i,j];
                                 arrayRandom[i,j] = max;
                             }          
 return arrayRandom;
@@ -64,9 +74,7 @@ return arrayRandom;
 
 int [,] arr = CreateArray2D();
 Print(arr);
-Print(ArrangeArray(arr));
-
-
+Print2(ArrangeArray(arr));
 
 
 
