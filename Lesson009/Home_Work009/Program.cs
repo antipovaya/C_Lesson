a3aafdbe0c66132e787bbeb1215caa52139c1c7e
Yana Antipova <antipovayana01@gmail.com>
2023-07-10 17:13:57 +0300

    Задача 68 Ф-я Аккермана

--- Lesson009/Home_Work009/Program.cs
+++ Lesson009/Home_Work009/Program.cs
@@ -46,9 +46,22 @@ int SumElementsFromMtoN (int m, int n)
 
 
 
-
-
 // Задача 68*: Напишите программу вычисления функции Аккермана 
 //с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+int Akkerman(int m, int n)
+{
+    if (m == 0){
+        return n + 1;
+    }
+    else if((m > 0) && (n == 0)){
+        return Akkerman(m - 1, 1);
+    }
+    else if((m > 0) && (n > 0)){
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+    return Akkerman(m,n);
+}
+Console.WriteLine(Akkerman(3,3));
